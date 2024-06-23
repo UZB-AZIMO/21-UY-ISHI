4931a6a739b47f72caeb12689a4902c0ee489230
UZB-AZIMO <abduvorisazimov712@gmail.com>
2024-06-23 21:37:50 +0500

    potfoliyi

---
+++ main.js
@@ -0,0 +1,113 @@
+// {1-masala
+
+// {
+//     function msMr(array){
+//         if ([array.gender] == "male"){
+//            console.log(`Janob   ${array.name}`);
+//            return "javob tayyor"
+//         }else{
+//             console.log(`Honim   ${array.name}`);
+//             return "javob tayyor"
+//         }
+//     }
+//     console.log( msMr({name: "ABROR", gender: "male"}))
+//     console.log( msMr({name: "ZUXRA", gender: "female"}))
+//     console.log( msMr({name: "AZIZ", gender: "male"}))
+// }
+
+// }
+
+
+// {2-masala
+
+// {
+// function card(cards, number) {
+//             if (number) {
+//                 return cards;
+//             } else {
+//                 let index = cards.slice(0, 4) + " **** **** **" + cards.slice(-2);
+//                 return index;
+//             }
+//         }
+    
+//         console.log(card("8600 1234 5651 2589", true));  // 8600 1234 5651 2589
+//         console.log(card("8600 5464 2332 4589", false));  // 8600 **** **** **89
+//         console.log(card("8600 4545 1231 8989", false));  // 8600 **** **** **89
+//     }
+    
+// }
+
+
+// {3-masala
+
+//    function perimetr(a,b) {
+//      let yegindi = 2;
+//         let a = yegindi * (a + b);
+//         console.log(a);
+//         return "natija tayyor"
+//       }
+//    console.log(perimetr(5, 6)); //2 * (5 + 6) = 22
+//    console.log(perimetr(12, 8)); //2 * (12 + 8) = 40
+ 
+
+// }
+
+
+// {4-masala
+
+// {
+//         const returnTrueFalse = (str) => str==="Ha" ? console.log(true) : console.log(false);
+//         returnTrueFalse()
+
+//         console.log(returnTrueFalse("Ha"));
+//       console.log(returnTrueFalse("Yo'q"));
+//       console.log(returnTrueFalse("Salom"));
+//     }
+
+// }
+
+
+// {5-masala
+
+// {
+//     function telNumber(tel) {
+//             if (tel == "+") {
+//               console.log(tel);
+//             } else {
+//               console.log(tel.padStart(16, "+"));
+//             }
+//             return "natija tayyor";
+//           }
+//           console.log(telNumber("+998 91 005 2674"));
+//           console.log(telNumber("998 91 005 2674"));
+//         }
+        
+
+// }
+
+
+// {6-masala
+
+// {
+//         const greeting = () =>  "Hello world";
+    
+//     console.log(greeting())
+//     }
+    
+//     {
+//       function brightness(input) {
+//         if (input === "dark") {
+//             return "light";
+//         } else if (input === "light") {
+//             return "dark";
+//         } else {
+//             return "Invalid input";
+//         }
+//     }
+    
+//     console.log(brightness("dark"));
+//     console.log(brightness("light"));
+//     }
+
+// }
+
